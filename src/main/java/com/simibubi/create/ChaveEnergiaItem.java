c8de8f25826b1688d22ef15c54b56b8c1aacc6e7
pedrohenrique77788 <pedrohenrique556692@gmail.com>
2026-06-24 04:54:58 -0300

    Create ChaveEnergiaItem.java

---
+++ src/main/java/com/simibubi/create/ChaveEnergiaItem.java
@@ -0,0 +1,13 @@
+package com.simibubi.create.content.equipment.wrench;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.Level;
+
+public class ChaveEnergiaItem extends Item {
+    // ... o resto do código da lógica dos cliques que te mandei antes
+
