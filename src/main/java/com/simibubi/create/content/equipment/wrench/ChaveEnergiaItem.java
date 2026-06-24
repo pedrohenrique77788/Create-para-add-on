ab83b69cc42d3492fe3cb81141d3619107967691
pedrohenrique77788 <pedrohenrique556692@gmail.com>
2026-06-24 05:51:46 -0300

    Update ChaveEnergiaItem.java

--- src/main/java/com/simibubi/create/content/equipment/wrench/ChaveEnergiaItem.java
+++ src/main/java/com/simibubi/create/content/equipment/wrench/ChaveEnergiaItem.java
@@ -1,3 +1,20 @@
+package com.simibubi.create.content.equipment.wrench;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class ChaveEnergiaItem extends Item {
+
+    public ChaveEnergiaItem(Properties properties) {
+        super(properties);
+    }
+
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Level level = context.getLevel();
@@ -69,4 +86,4 @@
         }
         return InteractionResult.PASS;
     }
-
+}
